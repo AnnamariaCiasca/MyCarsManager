e72a8d7a0d851f3a1f4294aa4516c264b0a52157
Valerio Carmine Ienco <48687112+IencoVC@users.noreply.github.com>
2019-06-08 20:39:20 +0200

    Aggiunti i fragmente la navigazione tra i fragment

---
+++ app/src/main/java/com/example/mycarsmanager/dashboard.kt
@@ -0,0 +1,47 @@
+package com.example.mycarsmanager
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class dashboard : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        account2.setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_account)}
+        garage2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_garage)}
+        spese2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_spese)}
+        cerca2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_cerca)}
+        park2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_park)}
+        service2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_service)}
+        security2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_security)}
+        options2.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_dashboard3_to_option)}
+
+    }
+}
